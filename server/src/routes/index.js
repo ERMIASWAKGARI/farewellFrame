1e6b8235742b6c9d77a906b8a996794d3846a981
Ermias Wakgari Yadeta <ermiaswakgarisse5@gmail.com>
2025-06-16 09:24:12 +0300

    project setup

---
+++ server/src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+// Import route modules here
+// Example: const authRoutes = require('./auth.routes');
+
+// Use route modules here
+// Example: router.use('/auth', authRoutes);
+
+module.exports = router
